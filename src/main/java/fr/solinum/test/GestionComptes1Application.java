e9534c11bd7f60c22d43151c8b6332f1bc35f1d4
ALI <ali.belhadj@niji.fr>
2018-05-09 00:46:37 +0200

    test sur le bon fonctionnement de l'insertion de l'entité client dans le database

--- src/main/java/fr/solinum/test/GestionComptes1Application.java
+++ src/main/java/fr/solinum/test/GestionComptes1Application.java
@@ -3,11 +3,22 @@ package fr.solinum.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-@EntityScan(basePackages= {"fr.solinum.entities"})
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import fr.solinum.dao.DaoClient;
+import fr.solinum.entities.Client;
+
+@EntityScan(basePackages = { "fr.solinum.entities" })
+@ComponentScan(basePackages = { "fr.solinum.dao" })
 @SpringBootApplication
 public class GestionComptes1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionComptes1Application.class, args);
+		ApplicationContext context = SpringApplication.run(GestionComptes1Application.class, args);
+		DaoClient daoClient = context.getBean(DaoClient.class);
+		daoClient.ajouterClient(new Client("Ali", "belhadj.ali@gmail.com"));
+		daoClient.ajouterClient(new Client("Rabii", "belhadj.rabii@gmail.com"));
+		daoClient.ajouterClient(new Client("Abdessalem", "belhadj.abdessalem@gmail.com"));
 	}
 }
